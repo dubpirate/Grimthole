e778c856e62ea0f6b2d4b27caeabc64be5bf0079
Spaxsta <devonkennedynz@yahoo.co.nz>
2018-05-28 21:08:43 +1200

    Add files via upload

---
+++ Source/Utils/SpriteManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Grimthole.Utils
+{ 
+    public abstract class SpriteManager
+    {
+        protected String name;
+        protected Color[] spriteData;
+        protected Texture2D sprite;
+        protected Rectangle rectangle;
+        protected int width;
+        protected int height;
+        protected SpriteManager(String name, Vector2 coords)
+        {
+            // Initialise base sprite details. 
+            this.name = name;
+            rectangle = new Rectangle((int)coords.X, (int)coords.Y, width, height);
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>(name);
+        }
+
+
+        public virtual void Update(GraphicsDeviceManager graphics, GameTime gt, ContentManager content)
+        {
+            // pass
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, rectangle, Color.White);
+        }
+
+        public String Name { get; protected set; }
+
+        public Rectangle GetRectangle { get; }
+    }
+}
