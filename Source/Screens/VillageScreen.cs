c594b26dfaa7351797a4f4fe159bcd068d6dafe8
William <w@torkington.com>
2018-06-01 14:59:11 +1200

    Major Screen Fixes + Minor Player Fixes
    Renamed SpashScreen -> VillageScreen.
    Fixed problem changing screens in ScreenManager.
    Converted all TileSize variables to the universal ScreenManager.Instance.TileSize.
    Changed player speed to be independent of how fast the GPU is.
    Added the MovementCommand for player movement.

---
+++ Source/Screens/VillageScreen.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grimthole.MacOS.Source.Utils;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Grimthole.MacOS.Source.Screens
+{
+    public class VillageScreen : GameScreen
+    {
+        Texture2D tile;
+        Point tileHeightAndWidth;
+		List<Point> points = new List<Point>();
+        
+        Player player;
+
+		public override void LoadContent()
+        {
+			base.LoadContent();
+            
+            player = new Player(new Vector2(200, 200));         
+            player.LoadContent(Content);         
+            
+            // The top left position of each background tile.
+			for (var i = 0; i < ScreenManager.Instance.Dimensions.Width; i += ScreenManager.Instance.TileSize)
+            {
+				for (var j = 0; j < ScreenManager.Instance.Dimensions.Height; j += ScreenManager.Instance.TileSize)
+                {
+                    points.Add(new Point(i, j));
+                }
+            }
+
+            // The height and width of the tiles.
+            tileHeightAndWidth = new Point(
+				ScreenManager.Instance.TileSize,
+				ScreenManager.Instance.TileSize
+            );         
+
+            tile = Content.Load<Texture2D>("Backgrounds/Grass-1");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+			player.Update(ScreenManager.Instance.Dimensions, gameTime, Content);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+		{
+            spriteBatch.Begin();
+            foreach (Point point in points)
+            {
+                spriteBatch.Draw(tile, new Rectangle(point, tileHeightAndWidth), Color.White);
+            }         
+            spriteBatch.End();
+
+			player.Draw(spriteBatch);
+        }
+	}
+}
---
+++ Source/Utils/MoveCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Grimthole.MacOS.Source.Utils
+{
+
+    /// <summary>
+	/// This class is uses the Command Software Design Pattern to take a Sprite
+	/// and move it in the desired direction by changing it's rectangle.
+    /// </summary>
+	public static class MoveCommand
+    {
+		public static void MoveUp(SpriteManager sprite, int delta)
+		{
+			sprite.SpriteDimensions = new Rectangle(
+				sprite.SpriteDimensions.X,
+				sprite.SpriteDimensions.Y - delta,
+				sprite.SpriteDimensions.Width,
+				sprite.SpriteDimensions.Height
+			);
+		}
+
+		public static void MoveDown(SpriteManager sprite, int delta)
+        {
+			sprite.SpriteDimensions = new Rectangle(
+				sprite.SpriteDimensions.X,
+				sprite.SpriteDimensions.Y + delta,
+				sprite.SpriteDimensions.Width,
+				sprite.SpriteDimensions.Height
+            );
+        }
+
+		public static void MoveLeft(SpriteManager sprite, int delta)
+        {
+			sprite.SpriteDimensions = new Rectangle(
+				sprite.SpriteDimensions.X - delta,
+				sprite.SpriteDimensions.Y,
+				sprite.SpriteDimensions.Width,
+				sprite.SpriteDimensions.Height
+            );
+        }
+
+		public static void MoveRight(SpriteManager sprite, int delta)
+        {
+			sprite.SpriteDimensions = new Rectangle(
+				sprite.SpriteDimensions.X + delta,
+				sprite.SpriteDimensions.Y,
+				sprite.SpriteDimensions.Width,
+				sprite.SpriteDimensions.Height
+            );
+        }
+    }
+}
