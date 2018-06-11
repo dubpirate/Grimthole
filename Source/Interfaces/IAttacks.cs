411a0309a03933a0d38672bd73e68b8e4302029e
William <w@torkington.com>
2018-06-12 11:27:48 +1200

    IAttacks extend IAbilities
    Both were identical, so I made IAttacks extend IAbilities.
    I also made Tick() take GameTime gameTime a parameter so we can get more accurate cooldown speed.

--- Source/Interfaces/IAttacks.cs
+++ Source/Interfaces/IAttacks.cs
@@ -1,26 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Grimthole.Interfaces
 {
     /// <summary>
     /// This interfaces is for all the abilities anyone can learn
     /// </summary>
-    public interface IAttacks
-    {
-        String Name { get; }
-
-        int Damage { get; }
-
-        float CoolDown { get; }
-
-        float RecoveryTime { get; }
-
-        void Tick();
+	public interface IAttacks : IAbilities
+	{
+		void MakeSound();
     }
-
 }
 
