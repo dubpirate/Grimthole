f5d49acea38ce1d4443417aba51be5321376a47d
William <w@torkington.com>
2018-07-03 22:32:31 +1200

    Added OSX namespacing

--- Source/Core/Tile.cs
+++ Source/Core/Tile.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Grimthole.Core
+namespace Grimthole.MacOS.Source.Core
 {
     public class Tile
     {
